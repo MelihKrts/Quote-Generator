05db1374733e71d6ba832110d958a36169461d82
MelihKrts <melih.karatas1281@gmail.com>
2024-10-24 18:10:51 +0300

    transition to different technology

--- Quote/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-    return (
-        <header className='header'>
-            <div className='logo'>Quoto Generator</div>
-        </header>
-    )
-}
